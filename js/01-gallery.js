0f9c3c211f4b40b566b9e4ca52b036187d9ad5d3
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-04-21 19:19:55 +0200

    Completed first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = createGalleryMarkup(galleryItems);
+galleryEl.addEventListener('click', onGalleryLinkClick);
+
+function onGalleryLinkClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) return;
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  window.addEventListener(
+    'keydown',
+    (evt) => {
+      if (evt.code !== 'Escape') return;
+      instance.close();
+    },
+    { once: true }
+  );
+}
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.reduce((markup, { original, preview, description }) => {
+    return markup.concat(`
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`);
+  }, '');
+}
